69d890af5a36b0c4b73b79fbec3c22207dd3f384
Saira <sairapinto28@outlook.com>
2021-12-01 00:17:53 -0600

    admin citas

--- admin.php
+++ admin.php
@@ -18,111 +18,65 @@ if(!isset($usuario)){
     <link rel="stylesheet" href="styles/normalize.css">
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap" rel="stylesheet">
 </head>
-
+<style>
+    #tabla{
+        margin: auto;
+        margin-top: 40px;
+        width: 60%;
+    }
+    .rounded{
+        background:white;
+        text-align: left;
+        border-collapse: collapse;
+        border-spacing: 10;
+        border-radius: 25px;
+        -moz-border-radius: 15px;
+        -webkit-border-radius: 15px;
+        width: 75%;
+    }
+    th, td{
+        padding: 15px;
+    }
+    thead{
+        background: #2997c3;
+    }
+    tr:nth-child(even){
+        background-color: #ddd;
+    }
+</style>
 <body>
     <?php include "logica/headerOthers.php"; ?>
-    <section class="contenedor">
-        <?php
-            require 'logica/conexion.php';
-
-            $idSession=$_SESSION['usuario'];
-                $sql="SELECT *
-                    FROM usuarios
-                    WHERE EMAIL='$idSession'";
-                $res=mysqli_query($conexion,$sql);
-                $num=mysqli_fetch_assoc($res);
-
-                
-
-        ?>
-        <h1>¡Realiza tu cita!</h1>
-        <div>
-            <h4>Cita</h4>
-            <form action="logica/hacerCita.php" method="POST">
-                <div>
-                    <input name="id" type="hidden" value="<?php echo $num['ID_USUARIO'];  ?>">
-                </div>
-                <div>
-                    <label>Tipo de cita</label>
-                    <select name="tipo">
-                        <option value="0">consulta</option>
-                        <option value="1">cirugia</option>
-                    </select>
-                </div>
-                <div>
-                <?php
-                    $sql= "SELECT * FROM doctores";
-
-                    $res=mysqli_query($conexion,$sql);
-                    $fila=mysqli_fetch_assoc($res);
-                ?>
-                    <label>Doctor</label>
-                    <select name="doctor">
-                        <?php  
-                            do{ ?>
-                            <option value="<?php echo $fila['id_doctor'] ?>">
-                                <?php echo $fila['name']."-"; echo $fila['especialidad'];?>
-                            </option>
-                        <?php
-                            }while($fila= mysqli_fetch_assoc($res)); 
-                        ?>
-                    </select>
-                </div>
-                <div>
-                    <label>Fecha</label>
-                    <input name="fecha" id="fecha" type="date" required>
-                </div>
-                <div>
-                    <label>Hora</label>
-                    <input name="hora" id="hora" type="time" required>
-                </div>
-                <button name="submit" type="submit" class="btn-login">Hacer cita</button>
-            </form>
-        </div>
-
-        
-    </section>
-
+    <?php require 'logica/conexion.php'; ?>
     <section class="contenedor">
         <h1>Mis citas</h1>
         <?php
-        $id=$num['ID_USUARIO'];
-		$sql= "SELECT * FROM cita WHERE id_usuario = $id";
+            $sql= "SELECT * FROM cita";
 
-		$res=mysqli_query($conexion,$sql);
-        $fila=mysqli_fetch_assoc($res);
-        
+            $res=mysqli_query($conexion,$sql);
+            $fila=mysqli_fetch_assoc($res);
 		?>
-    <div id="tabla">
-	 <table>
-        <thead>
-                <th>Tipo</th><th>Doctor</th><th>Fecha</th><th>Hora</th><th>Usuario</th>
-        </thead>
-	 	<?php  
-	 		do 
-	 		{
-	 	?>
-	 	<tr>
-	 		<td><?php echo $fila['tipo'] ?></td>
-			<td><?php echo $fila['doctor'] ?></td>
-            <td><?php echo $fila['fecha'] ?></td>
-            <td><?php echo $fila['hora'] ?></td>
-            <td><?php echo $fila['id_usuario'] ?></td>
-	 	</tr>
-	 	<?php
-	 		}while($fila= mysqli_fetch_assoc($res)); 
-	 	 ?>
+        <div id="tabla">
+            <table align=center class="rounded">
+                <thead>
+                        <th>Tipo</th><th>Doctor</th><th>Fecha</th><th>Hora</th><th>Usuario</th>
+                </thead>
+                <?php  
+                    do 
+                    {
+                ?>
+                <tr>
+                    <td><?php echo $fila['tipo'] ?></td>
+                    <td><?php echo $fila['doctor'] ?></td>
+                    <td><?php echo $fila['fecha'] ?></td>
+                    <td><?php echo $fila['hora'] ?></td>
+                    <td><?php echo $fila['id_usuario'] ?></td>
+                </tr>
+                <?php
+                    }while($fila= mysqli_fetch_assoc($res)); 
+                ?>
+            </table>
+        </div>
+        <br> <br> <br> <br>
     </section>
-
-    <script>
-        function ocultar(){
-            var tipo = document.getElementById("tipo").value;
-            if(tipo == 0){
-                document.getElementById("cirugia").style.display = "none";
-            }else{
-                document.getElementById("consulta").style.display = "none";
-            }
-        }
-    </script>
 </body>
 </html>
